4be4f2ccea42884d9f9783faf886f89e138cf02e
VincentPetitPro <48692227+VincentPetitPro@users.noreply.github.com>
2020-09-30 10:23:07 +0200

    2.7) mais affichage nom jour  ne marche pas

--- client/vue-application.js
+++ client/vue-application.js
@@ -1,16 +1,33 @@
-const HelloOtherWorld = window.httpVueLoader('./components/HelloOtherWorld.vue')
-
-Vue.component('hello-world', {
-  data: function () {
-    return {
-      message: ', World'
-    }
-  },
-  template: `<p>Hello{{ message }}!</p>`
-})
+const HelloOtherWorld = window.httpVueLoader("./components/HelloOtherWorld.vue");
 
 var app = new Vue({
-  el: '#app',
-  data: { },
-  components: { HelloOtherWorld }
-})
+	el: "#app",
+	data: {
+		selectedMonth: new Date().getMonth() + 1,
+		selectedYear: new Date().getFullYear(),
+	},
+	methods: {
+		getDaysInMonth: function (month, year) {
+			return new Date(year, month, 0).getDate();
+		},
+		daysOfMonth: function (month, year) {
+			tab = [];
+			days = this.getDaysInMonth(month, year);
+			for (let i = 0; i < days; i++) {
+				tab.push(i);
+			}
+			return tab;
+		},
+		dayName: function (day, month, year) {
+			days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+			dayDate = new Date(year, month - 1, day).getDay() % 7;
+			return days[dayDate];
+		},
+	},
+	computed: {
+		years: function () {
+			return Array.from({ length: 2100 - 1900 }, (value, index) => 1901 + index);
+		},
+	},
+	components: { HelloOtherWorld },
+});
