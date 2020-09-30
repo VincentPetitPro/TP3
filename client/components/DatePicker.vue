d42cb6225e8048dbcfcd8ca0cb4cb3f96658244a
VincentPetitPro <48692227+VincentPetitPro@users.noreply.github.com>
2020-09-30 23:09:57 +0200

    composant fonctionnel, manque props

---
+++ client/components/DatePicker.vue
@@ -0,0 +1,68 @@
+<template>
+	<div>
+		<p>Il y a {{ getDaysInMonth(this.selectedMonth, this.selectedYear) }} jours dans ce mois.</p>
+		<ul>
+			<li
+				v-bind:key="day"
+				style="list-style: none"
+				v-for="day in daysOfMonth(this.selectedMonth, this.selectedYear)"
+			>
+				{{ day + 1 }} : {{ dayName(day) }}
+			</li>
+		</ul>
+		<select v-model="selectedMonth">
+			<option value="1">January</option>
+			<option value="2">February</option>
+			<option value="3">March</option>
+			<option value="4">April</option>
+			<option value="5">May</option>
+			<option value="6">June</option>
+			<option value="7">July</option>
+			<option value="8">August</option>
+			<option value="9">September</option>
+			<option value="10">October</option>
+			<option value="11">November</option>
+			<option value="12">December</option>
+		</select>
+		<select v-model="selectedYear">
+			<option v-bind:key="year" v-for="year in years" :value="year">{{ year }}</option>
+		</select>
+	</div>
+</template>
+
+<script>
+module.exports = {
+	data: function () {
+		return {
+			selectedMonth: new Date().getMonth() + 1,
+			selectedYear: new Date().getFullYear(),
+		};
+	},
+	props: {},
+	methods: {
+		getDaysInMonth: function (month, year) {
+			return new Date(year, month, 0).getDate();
+		},
+		daysOfMonth: function (month, year) {
+			tab = [];
+			days = this.getDaysInMonth(month, year);
+			for (let i = 0; i < days; i++) {
+				tab.push(i);
+			}
+			return tab;
+		},
+		dayName: function (day) {
+			days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+			dateURL =
+				this.selectedMonth + "/" + (parseInt(day, 10) + parseInt(1, 10)) + "/" + this.selectedYear;
+			dayDate = new Date(dateURL).getDay();
+			return days[dayDate];
+		},
+	},
+	computed: {
+		years: function () {
+			return Array.from({ length: 2100 - 1900 }, (value, index) => 1901 + index);
+		},
+	},
+};
+</script>
--- client/vue-application.js
+++ client/vue-application.js
@@ -1,35 +1,14 @@
 const HelloOtherWorld = window.httpVueLoader("./components/HelloOtherWorld.vue");
+const DatePicker = window.httpVueLoader("./components/datePicker.vue");
 
 var app = new Vue({
 	el: "#app",
 	data: {
-		selectedMonth: new Date().getMonth() + 1,
-		selectedYear: new Date().getFullYear(),
+		parentSelectedMonth: new Date().getMonth() + 1,
+		parentSelectedYear: new Date().getFullYear(),
 	},
-	methods: {
-		getDaysInMonth: function (month, year) {
-			return new Date(year, month, 0).getDate();
-		},
-		daysOfMonth: function (month, year) {
-			tab = [];
-			days = this.getDaysInMonth(month, year);
-			for (let i = 0; i < days; i++) {
-				tab.push(i);
-			}
-			return tab;
-		},
-		dayName: function (day) {
-			days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-			dateURL =
-				this.selectedMonth + "/" + (parseInt(day, 10) + parseInt(1, 10)) + "/" + this.selectedYear;
-			dayDate = new Date(dateURL).getDay();
-			return days[dayDate];
-		},
+	components: {
+		HelloOtherWorld,
+		DatePicker,
 	},
-	computed: {
-		years: function () {
-			return Array.from({ length: 2100 - 1900 }, (value, index) => 1901 + index);
-		},
-	},
-	components: { HelloOtherWorld },
 });
