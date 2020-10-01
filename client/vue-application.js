cdb54216e41a1fe869a6ad33d9530258cecf29c8
VincentPetitPro <48692227+VincentPetitPro@users.noreply.github.com>
2020-10-01 04:32:31 +0200

    Erreur de compréhension, retour au 2.7)

--- client/vue-application.js
+++ client/vue-application.js
@@ -1,11 +1,35 @@
-const HelloOtherWorld = window.httpVueLoader("./components/HelloOtherWorld.vue");
-const DatePicker = window.httpVueLoader("./components/datePicker.vue");
+//const HelloOtherWorld = window.httpVueLoader("./components/HelloOtherWorld.vue");
 
 var app = new Vue({
 	el: "#app",
 	data: {
-		parentSelectedMonth: new Date().getMonth() + 1,
-		parentSelectedYear: new Date().getFullYear(),
+		selectedMonth: new Date().getMonth() + 1,
+		selectedYear: new Date().getFullYear(),
 	},
-	components: { DatePicker },
+	methods: {
+		getDaysInMonth: function (month, year) {
+			return new Date(year, month, 0).getDate();
+		},
+		daysOfMonth: function (month, year) {
+			tab = [];
+			days = this.getDaysInMonth(month, year);
+			for (let i = 0; i < days; i++) {
+				tab.push(i);
+			}
+			return tab;
+		},
+		dayName: function (day) {
+			days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+			dateURL =
+				this.selectedMonth + "/" + (parseInt(day, 10) + parseInt(1, 10)) + "/" + this.selectedYear;
+			dayDate = new Date(dateURL).getDay();
+			return days[dayDate];
+		},
+	},
+	computed: {
+		years: function () {
+			return Array.from({ length: 2100 - 1900 }, (value, index) => 1901 + index);
+		},
+	},
+	components: {},
 });
